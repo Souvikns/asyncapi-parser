4815692de3066809c56702b59362a6121c11e342
Souvik De <souvikde.ns@gmail.com>
2025-01-17 17:57:05 +0530

    chore: formatting

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,4 +1,4 @@
 pub mod asyncapi;
 pub mod info;
-pub mod server;
 pub mod security;
+pub mod server;
--- src/models/security.rs
+++ src/models/security.rs
@@ -1,18 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Security {
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename="type")]
+    #[serde(rename = "type")]
     tp: Option<SecuritySchemeType>,
     #[serde(skip_serializing_if = "Option::is_none")]
     description: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename="in")]
+    #[serde(rename = "in")]
     inn: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     scheme: Option<String>,
@@ -23,13 +22,13 @@ pub struct Security {
     #[serde(skip_serializing_if = "Option::is_none")]
     flows: Option<OAuthFlows>, // TODO: Finish Oauthflows
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    scopes: Vec<String>
+    scopes: Vec<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize,  PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type")]
 enum SecuritySchemeType {
-    #[serde(rename="userPassword")]
+    #[serde(rename = "userPassword")]
     UserPassword,
     #[serde(rename = "apiKey")]
     ApiKey,
@@ -53,12 +52,9 @@ enum SecuritySchemeType {
     #[serde(rename = "scramSha512")]
     ScramSha512,
     #[serde(rename = "gssapi")]
-    Gssapi
+    Gssapi,
 }
-
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct OAuthFlows {
-    
-}
+pub struct OAuthFlows {}
